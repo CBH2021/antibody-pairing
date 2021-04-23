fd3f4a09ff35139a2385dc77d4b0abe16ac607b1
mads-br <52624217+mads-br@users.noreply.github.com>
2021-04-23 22:58:56 +0200

    Small fixes for benchmarking

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,6 @@
 import argparse, joblib
-
 import pandas as pd
-import sklearn as sk
-
-from . import extra_funcs
+import extra_funcs
 
 # set up argument parsing (make sure these match those in config.yml)
 parser = argparse.ArgumentParser()
@@ -18,18 +15,9 @@ aligned = extra_funcs.align_abs(data.Hchain, data.Lchain)
 encoded = extra_funcs.one_hot_encode(aligned.Hchain_align, aligned.Lchain_align)
 
 # PREDICT
-modelfile = '../finalized_model.sav'
-loaded_model = joblib.load(filename)
+modelfile = 'finalized_model.sav'
+loaded_model = joblib.load(modelfile)
 y_pred = loaded_model.predict_proba(encoded)
 
-
-
 # SAVE PREDICTIONS WITH THE COLUMN NAME prediction IN THE FILE predictions.csv
-pd.DataFrame(y_pred[:,1], columns=['prediction']).to_csv("predictions.csv", index=False)
-
-
-
-
-
-
-
+pd.DataFrame(y_pred[:, 1], columns=['prediction']).to_csv("predictions.csv", index=False)
